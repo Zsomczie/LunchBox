3ece8dd4a9894d156c29591ad71a4e1abe79af6b
Zsomczie <zsomi.f@gmail.com>
2023-05-16 12:18:26 +0300

    3 different weapons, reload, mag ammo

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
 
 public class Shooting : MonoBehaviour
 {
+    public struct weaponData
+    {
+        public string weaponName;
+        public string weaponType;
+        public int magCapacity;
+        public int maxAmmo;
+        public float FiringTime;
+        public float damage;
+        public float reloadTime;
+        public int currentAmmo;
+        public weaponData(string name,string type, int magcap, int maxammo, float time, float dmg,float reload,int ammo) 
+        {
+            weaponName = name;
+            weaponType = type;
+            magCapacity = magcap;
+            maxAmmo = maxammo;
+            FiringTime = time;
+            damage = dmg;
+            reloadTime = reload;
+            currentAmmo = ammo;
+        }
+    }
     Camera mainCam;
     [SerializeField]Vector3 mousePos;
     public GameObject bullet;
@@ -11,10 +35,25 @@ public class Shooting : MonoBehaviour
     public bool canFire=true;
     private float timer;
     public float timeBetweenFiring= 0.5f;
+    public int weaponNumber;
+    public List<weaponData> weaponDatas = new List<weaponData>();
+    public weaponData equippedWeapon;
+    public int currentMagAmmo;
+    public bool reloading;
+    public bool empty=false;
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        string[] lines = File.ReadAllLines("Assets/Data/weaponinfo.txt");
+        foreach (var line in lines)
+        {
+            string[] oneLine = line.Split();
+            weaponDatas.Add(new weaponData(oneLine[0], oneLine[1], int.Parse(oneLine[2]), int.Parse(oneLine[3]), float.Parse(oneLine[4]),float.Parse(oneLine[5]),float.Parse(oneLine[6]),int.Parse(oneLine[7])));
+        }
+        Debug.Log(weaponDatas[1].FiringTime);
+        equippedWeapon = weaponDatas[weaponNumber];
+        equippedWeapon.currentAmmo = equippedWeapon.magCapacity;
     }
 
     // Update is called once per frame
@@ -27,16 +66,71 @@ public class Shooting : MonoBehaviour
         if (!canFire)
         {
             timer += Time.deltaTime;
-            if (timer>timeBetweenFiring)
+            if (timer>equippedWeapon.FiringTime)
             {
                 canFire = true;
                 timer = 0;
             }
         }
-        if (Input.GetMouseButton(0)&&canFire)
+        //Debug.Log(equippedWeapon.damage);
+        if (Input.mouseScrollDelta.y<0)
+        {
+            weaponDatas[weaponNumber] = equippedWeapon;
+            if (weaponNumber==weaponDatas.Count-1)
+            {
+                weaponNumber = 0;
+            }
+            else
+            {
+                weaponNumber++;
+            }
+            
+            equippedWeapon = weaponDatas[weaponNumber];
+            //currentMagAmmo = equippedWeapon.magCapacity;
+        }
+
+        if (Input.mouseScrollDelta.y>0)
+        {
+            weaponDatas[weaponNumber] = equippedWeapon;
+            if (weaponNumber==0)
+            {
+                weaponNumber = weaponDatas.Count - 1;
+            }
+            else
+            {
+                weaponNumber--;
+            }
+            equippedWeapon = weaponDatas[weaponNumber];
+            //currentMagAmmo = equippedWeapon.magCapacity;
+        }
+        if (Input.GetKeyDown(KeyCode.R) && currentMagAmmo != equippedWeapon.magCapacity && equippedWeapon.weaponType != "beam"&&!reloading
+            || Input.GetMouseButtonDown(0) && equippedWeapon.currentAmmo == 0 && !reloading && equippedWeapon.weaponType != "beam"&&empty)
+        {
+            IEnumerator reload()
+                {
+                reloading = true;
+                yield return new WaitForSeconds(equippedWeapon.reloadTime);
+                equippedWeapon.currentAmmo = equippedWeapon.magCapacity;
+                reloading = false;
+                empty = false;
+            }
+            StartCoroutine(reload());
+            
+        }
+        if (Input.GetMouseButton(0) && canFire && equippedWeapon.currentAmmo != 0&&!reloading)
         {
             canFire = false;
             Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+            equippedWeapon.currentAmmo--;
+            IEnumerator setEmpty() 
+            {
+                yield return new WaitForSeconds(0.1f);
+                empty = true;
+            }
+            if (equippedWeapon.currentAmmo==0)
+            {
+                StartCoroutine(setEmpty());
+            }
         }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,12 +7,15 @@ public class UIManager : MonoBehaviour
 {
     public TMP_Text HealthText;
     public TMP_Text KeyText;
+    public TMP_Text magText;
     PlayerController player;
+    Shooting shooting;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
+        shooting = GameObject.Find("RotatePoint").GetComponent<Shooting>();
     }
 
     // Update is called once per frame
@@ -20,5 +23,6 @@ public class UIManager : MonoBehaviour
     {
         HealthText.text = player.health.ToString()+" Health";
         KeyText.text = player.keys.ToString()+ " Keys";
+        magText.text = shooting.equippedWeapon.currentAmmo.ToString() + "/" + shooting.equippedWeapon.magCapacity;
     }
 }
